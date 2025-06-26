4d016324f468d3c644330b2d3cce292ca871fcce
unknown <olehidavis@gmail.com>
2025-06-26 17:57:01 +0100

    testing 3

--- reminderx_backend/settingsprod.py
+++ reminderx_backend/settingsprod.py
@@ -186,8 +186,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # For development
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.hostinger.com"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
